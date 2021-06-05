65403cd03c398f1687be8f2459dd8dddfc2b9c84
Pranjal Pandey <pranjalpandey92@gmail.com>
2021-06-05 20:45:41 +0545

    day 1 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,20 +11,46 @@ class LoginPage extends StatelessWidget {
               'assets/images/login.png',
               fit: BoxFit.cover,
             ),
-
             SizedBox(
               height: 20,
             ),
-
-            Text('Welcome to Flutter Catalog', style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),),
-
+            Text(
+              'Welcome to Flutter Catalog',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             SizedBox(
               height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                RaisedButton(
+                  onPressed: (){
+                    print('button is pressed...');
+                  },
+                  child: Text('Login'),
+                  color: Colors.purple[800],
+                  textColor: Colors.white,
+                ),
+                ],
+              ),
             )
-
           ],
         ));
   }
